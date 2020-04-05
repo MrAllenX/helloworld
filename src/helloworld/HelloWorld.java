cdf2eddda10b17169661950648c0fd123ae07a44
MrAllenX <1095686801@qq.com>
2020-04-05 16:13:50 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hell world");
+		System.out.println("Hell world  Ëæ±ãÐÞ¸ÄÖ®ºó");
 
 	}
 
